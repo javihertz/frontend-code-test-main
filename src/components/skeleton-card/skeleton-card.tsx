7db2a65917cb9b0c3cb1d779bdddfdf244d916a8
javihertz <javicasados@gmail.com>
2024-07-12 09:45:15 +0200

    feat(skeleton-card): create component

---
+++ src/components/skeleton-card/skeleton-card.tsx
@@ -0,0 +1,17 @@
+import './skeleton-card.css';
+
+export function SkeletonCard() {
+  return (
+    <div className='skeleton-container'>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+      <div className='skeleton'></div>
+    </div>
+  );
+}
