8872833112222460315331093004204f08c086bf
javihertz <javicasados@gmail.com>
2024-07-12 15:38:44 +0200

    refactor(main): autosave format changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "urql";
-import { createClient } from "urql";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import PersonPage from "./pages/PersonPage";
-import HomePage from "./pages/HomePage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createClient, Provider } from 'urql';
+import PersonPage from './pages/PersonPage';
+import Home from './pages/home/home';
 
 const client = createClient({
-  url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+  url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
 });
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    path: '/',
+    element: <Home />,
   },
   {
-    path: "/person/:personId",
+    path: '/person/:personId',
     element: <PersonPage />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider value={client}>
       <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
