c6317a6ae418ef0c836392a11aac56f9c5f95d06
javihertz <javicasados@gmail.com>
2024-07-13 23:34:03 +0200

    feat(person): create interfaces

---
+++ src/pages/person/interfaces/person.interface.ts
@@ -0,0 +1,39 @@
+export interface PersonDTO {
+  person: {
+    birthYear: string;
+    name: string;
+    species: {
+      averageHeight?: string;
+    };
+    filmConnection: {
+      films: [
+        {
+          releaseDate: string;
+          title: string;
+          producers: string[];
+          planetConnection: {
+            planets: [
+              {
+                surfaceWater: number;
+              }
+            ];
+          };
+        }
+      ];
+    };
+  };
+}
+
+export interface PersonMap {
+  averageHeight: string;
+  filmsList: {
+    planetWithoutWater: number;
+    releaseDate: string;
+    title: string;
+  }[];
+  personName: string;
+  producersList: {
+    name: string;
+    timesWorked: number;
+  }[];
+}
