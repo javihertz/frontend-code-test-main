055a0d6308f6916522e164de17cd824b31e10656
javihertz <javicasados@gmail.com>
2024-07-13 23:35:11 +0200

    feat(use-person-map): implement use memo map

---
+++ src/pages/person/hooks/use-person-map.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from 'react';
+import { PersonDTO, PersonMap } from '../interfaces/person.interface';
+
+type ProducersList = {
+  [k: string]: {
+    name: string;
+    timesWorked: number;
+  };
+};
+
+export function usePersonMap(data: PersonDTO | undefined) {
+  return useMemo(() => {
+    if (!data?.person) {
+      return undefined;
+    }
+
+    const producersList = data.person.filmConnection.films.reduce(
+      (acc: ProducersList, film) => {
+        film.producers.forEach((producer) => {
+          if (acc[producer]) {
+            acc[producer].timesWorked++;
+          } else {
+            acc[producer] = { name: producer, timesWorked: 1 };
+          }
+        });
+        return acc;
+      },
+      {}
+    );
+
+    const filmsList = data.person.filmConnection.films.map((film) => ({
+      title: film.title,
+      releaseDate: film.releaseDate,
+      planetWithoutWater: film.planetConnection.planets.filter(
+        (planet) => planet.surfaceWater === 0
+      ).length,
+    }));
+
+    const personMap: PersonMap = {
+      personName: data.person.name,
+      producersList: Object.entries(producersList).map(([, info]) => info),
+      averageHeight: data.person.species?.averageHeight ?? 'Missing Data',
+      filmsList,
+    };
+
+    return personMap;
+  }, [data]);
+}
