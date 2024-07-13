fce835fcc1a25a2d6f42f4ffc094774e96d0ec90
javihertz <javicasados@gmail.com>
2024-07-13 23:33:39 +0200

    feat(people-list): create component

--- src/pages/home/components/people-list/people-list.tsx
+++ src/pages/home/components/people-list/people-list.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
+import { Card } from '../../../../components/card/card';
 import { HomeQuery } from '../../../../generated/graphql';
+import './people-list.css';
 
 interface PeopleListProps {
   people: HomeQuery['allPeople'];
@@ -8,25 +10,24 @@ interface PeopleListProps {
 export function PeopleList({ people }: PeopleListProps) {
   const navigate = useNavigate();
 
-  const handleOnClickPerson = (personId: string | undefined) => {
+  const handleOnClickPerson = (personId: string | undefined, index: number) => {
     if (!personId) {
-      throw new Error('ERROR_MISSING_PERSON_ID'); // NOTE: It can be handled by the errorElement of react router dom
+      throw new Error('ERROR_MISSING_PERSON_ID'); // NOTE: It can be handled by the errorElement of react router dom, having a dictionary to show custom errors
     }
 
-    navigate(`/person/${personId}`);
+    navigate(`/person/${personId}?personNumber=${index + 1}`);
   };
 
   return (
-    <ul className='people'>
-      {people?.edges?.map((person) => (
-        <li
-          className='person'
+    <div className='people'>
+      {people?.edges?.map((person, index) => (
+        <Card
           key={person?.node?.id}
-          onClick={() => handleOnClickPerson(person?.node?.id)}
+          onCallbackClick={() => handleOnClickPerson(person?.node?.id, index)}
         >
           <p className='person-name'>{person?.node?.name ?? 'Missing Name'}</p>
-        </li>
+        </Card>
       ))}
-    </ul>
+    </div>
   );
 }
