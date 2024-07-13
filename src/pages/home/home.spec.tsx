0e1cc5bc88bc3fb0f77c5e089313e93d90eff06d
javihertz <javicasados@gmail.com>
2024-07-14 01:30:24 +0200

    test(home): cover cases

---
+++ src/pages/home/home.spec.tsx
@@ -0,0 +1,80 @@
+import { screen } from '@testing-library/react';
+import { customRender } from '../../utils/custom-render';
+import Home from './home';
+
+const mockData = jest.fn().mockReturnValue({
+  edges: [
+    {
+      node: {
+        id: 'cGVvcGxlOjE=',
+        name: 'Luke Skywalker',
+      },
+    },
+    {
+      node: {
+        id: 'cGVvcGxlOjI=',
+        name: 'C-3PO',
+      },
+    },
+  ],
+});
+
+const mockIsLoading = jest.fn().mockReturnValue(false);
+const mockHasError = jest.fn().mockReturnValue(false);
+
+jest.mock('./hooks/use-home.tsx', () => ({
+  useHome: () => ({
+    data: mockData(),
+    hasError: mockHasError(),
+    isLoading: mockIsLoading(),
+  }),
+}));
+
+describe('Home', () => {
+  describe('when is loading the data', () => {
+    it('should show a loader', () => {
+      mockIsLoading.mockReturnValueOnce(true);
+
+      customRender(<Home />);
+
+      const loader = screen.getByTestId('loader');
+
+      expect(loader).toBeInTheDocument();
+    });
+  });
+
+  describe('when has an error loading the data', () => {
+    it('should show an error message', () => {
+      mockHasError.mockReturnValueOnce(true);
+
+      customRender(<Home />);
+
+      const errorMessage = screen.getByText(/unexpected error/i);
+
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
+  describe('when has data to be shown', () => {
+    it('should show the people list', () => {
+      customRender(<Home />);
+
+      const peopleList = screen.getByTestId('people-list');
+
+      expect(peopleList).toBeInTheDocument();
+    });
+  });
+
+  describe('when the data is empty', () => {
+    it('should show an empty message', () => {
+      mockData.mockReturnValueOnce({
+        edges: [],
+      });
+      customRender(<Home />);
+
+      const emptyMessage = screen.getByText(/no people to be shown/i);
+
+      expect(emptyMessage).toBeInTheDocument();
+    });
+  });
+});
