cf504aebb9a7bb276f79436ce5f6a5caa376c8dd
javihertz <javicasados@gmail.com>
2024-07-14 01:01:19 +0200

    refactor(people-list): remove throw

--- src/pages/home/components/people-list/people-list.tsx
+++ src/pages/home/components/people-list/people-list.tsx
@@ -11,10 +11,6 @@ export function PeopleList({ people }: PeopleListProps) {
   const navigate = useNavigate();
 
   const handleOnClickPerson = (personId: string | undefined, index: number) => {
-    if (!personId) {
-      throw new Error('ERROR_MISSING_PERSON_ID'); // NOTE: It can be handled by the errorElement of react router dom, having a dictionary to show custom errors
-    }
-
     navigate(`/person/${personId}?personNumber=${index + 1}`);
   };
 
@@ -22,7 +18,7 @@ export function PeopleList({ people }: PeopleListProps) {
     <div className='people'>
       {people?.edges?.map((person, index) => (
         <Card
-          key={person?.node?.id}
+          key={person?.node?.id ?? index}
           onCallbackClick={() => handleOnClickPerson(person?.node?.id, index)}
         >
           <p className='person-name'>{person?.node?.name ?? 'Missing Name'}</p>
